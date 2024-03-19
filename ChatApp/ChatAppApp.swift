75f1cfcd817e264ee3d1d2cc14c9c1a02007b0ed
Om Shah <omdshah2004@gmail.com>
2024-03-18 23:34:58 -0500

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Shah, Om on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
